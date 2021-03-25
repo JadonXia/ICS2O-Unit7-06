9df7c50c27fd1fbe92756d4514530e6fd1c7dc72
JadonXia <jadon.xia@ocsbstudent.ca>
2021-03-25 14:56:23 +0000

    added parseInt

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 // These are the variables I will need for my loop.
-let age = 15
+const age = 15
 let guess = 0
 
 // Below is my while loop for 'guess my age'.
-while (guess != age) {
+while (guess !== age) {
   guess = prompt('To access this site, guess my age!')
-  if (guess == age) {
+  guess = parseInt(guess)
+  if (guess === age) {
   alert('Congratulations, you got it!')
   } else if (guess < age) {
     alert('You guessed too low!')
   } else if (guess > age) {
     alert('You guessed too high!')
   } else {
-    alert('Please enter a number, or close this site.')
+    alert('Please enter a number, or close the site.')
   }
 }
